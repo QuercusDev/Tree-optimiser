2c5c1a2216785deb7ea85b1a7b155e54637c63a1
QuercusDev <63300993+QuercusDev@users.noreply.github.com>
2020-04-07 15:52:43 +0100

    Startup
    Describing methods required and what they will do

---
+++ src/com/company/Main.java
@@ -0,0 +1,41 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+Scanner hHead = new Scanner(System.in);
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+    public static double tree() {
+        //this is where the tree measurements will be input
+
+        return 0;
+    }
+
+    public static double priceMatrix() {
+        //this is where the price matrix will be stored length, diameter and value
+        return 0;
+    }
+
+    public static double treeVol() {
+        //this is where the tree volume will be calculated
+        //merchantable volume only so it will look like a flat head cone
+        return 0;
+    }
+
+    public static double logVol() {
+        //this is where the log volumes will be calculated
+        //log volume 
+        return 0;
+    }
+
+    public static double smallEndDia() {
+
+        return 0;
+    }
+
+    }
+
