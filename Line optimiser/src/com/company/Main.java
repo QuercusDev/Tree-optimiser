2b51b09533365c1eeea3279b796011dbf65bf135
QuercusDev <63300993+QuercusDev@users.noreply.github.com>
2020-04-08 15:09:32 +0100

    Updated prints

--- Line optimiser/src/com/company/Main.java
+++ Line optimiser/src/com/company/Main.java
@@ -6,5 +6,28 @@ public class Main {
 	System.out.println("Please enter the legnth of your line: ");
 	Scanner lineLength = new Scanner(System.in);
 
-    }
-}
+        /*
+        double a = 4.9;
+        double b = 3.7;
+        double c = 2.5;
+        double d = 1.9;
+        double e = 3.0;
+         */
+
+        int [] numberOfCuts = {4,3,2,1,3};
+        int smallestModulo=0;
+        int newCombo=0;
+
+        for (int i = 0; smallestModulo % numberOfCuts[i]; i++) {
+            if (smallestModulo > newCombo) {
+                smallestModulo = newCombo;
+            }
+        }
+
+
+
+
+        }
+        }
+
+
