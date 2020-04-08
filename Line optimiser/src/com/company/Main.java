c512b9e128ac3fba4e5742b02c9c598117ef69ca
QuercusDev <63300993+QuercusDev@users.noreply.github.com>
2020-04-08 17:26:22 +0100

    Got it working

--- Line optimiser/src/com/company/Main.java
+++ Line optimiser/src/com/company/Main.java
@@ -1,33 +1,43 @@
 package com.company;
+import java.util.Arrays;
 import java.util.Scanner;
-public class Main {
+public class Main{
 
     public static void main(String[] args) {
-	System.out.println("Please enter the legnth of your line: ");
-	Scanner lineLength = new Scanner(System.in);
+        System.out.println("Please enter the legnth of your line: ");
+        Scanner keyboard = new Scanner(System.in);
 
-        /*
-        double a = 4.9;
-        double b = 3.7;
-        double c = 2.5;
-        double d = 1.9;
-        double e = 3.0;
-         */
+        double lineLength = keyboard.nextDouble();
+        double[] a = {4.9, 3.7, 3.1, 2.5, 1.9, 3.0};
 
-        int [] numberOfCuts = {4,3,2,1,3};
-        int smallestModulo=0;
-        int newCombo=0;
+        System.out.println(findMin(a));
+        double minCutLength = findMin(a);
 
-        for (int i = 0; smallestModulo % numberOfCuts[i]; i++) {
-            if (smallestModulo > newCombo) {
-                smallestModulo = newCombo;
+
+        int maxNumberOfCuts = (int) (lineLength / minCutLength);
+        System.out.format("%d",maxNumberOfCuts);
+
+
+    }
+    public static double findMin(double[] a) {
+        Arrays.sort(a);
+        double minCutLength = (a[0]);
+        return minCutLength;
+    }
+}
+/*
+        double temp;
+
+        for (int i = 0; i < a.length; i++) {
+            for (int j = i + 1; j < a.length - 1; j++) {
+                if (a[i] > a[j]) {
+                    temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
+                double smallestCut = a[i];
+                System.out.println(smallestCut);
             }
-        }
-
-
-
-
-        }
-        }
+*/
 
 
